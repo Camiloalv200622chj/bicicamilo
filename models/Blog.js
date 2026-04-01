53f9a558bf463359d7aa5e632cd7406746cd32d7
Camiloalv200622chj <juancamiloalvarezsarmiento22@gmail.com>
2026-04-01 16:28:55 -0500

    Add files via upload

---
+++ models/Blog.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  image: { type: String, required: true },
+  summary: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: String, default: 'Bicicamilo Official' },
+  category: { type: String, default: 'Noticias' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
